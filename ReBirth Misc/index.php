8e81d567a1eb0763bf7610900a0580ed9814b7c5
Kurt Kurasaki <kurt@peff.com>
2026-03-01 08:18:51 -0800

    add files to main

---
+++ ReBirth Misc/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Peff's ReBirth Mods, Songs, and ReBirth RB-338 Utilities Page</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="Peff, MP3, Kurt Kurasaki, TB-303 Samples, ReBirth RB-338 Mods, Analogue Synthesis">
+<meta name="description" content="Peff's ReBirth Mods, Songs, and ReBirth RB-338 Utilities Page.  Website of Kurt Kurasaki, a.k.a Peff.  Audio Resources for electronic music and ReBirth RB-338 and Peff's MP3 music.">
+</head>
+
+<?php 
+include 'http://www.peff.com/journal/rebirth-archive/' 
+?>
+
+</html>
